b261e8ea2827a7d6205db4bd0ed36990e1f14274
renjm <jianmin.ren@jfjun.com>
2018-11-20 23:05:54 +0800

    添加配置文件

--- client.js
+++ client.js
@@ -1,41 +1,13 @@
 const io = require('socket.io-client');
-// var chat = require('socket.io')('4000');
-const chat = io('http://13.209.42.70:4000')
+const chat = io('http://127.0.0.1:4000')
 const readline = require('readline');
-const mongoose = require('mongoose');
 var ObjectID = require("bson-objectid");
-// const objectid = require('objectid')
-const options = {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    poolSize: 5, // 连接池中维护的连接数
-    reconnectTries: Number.MAX_VALUE,
-    keepAlive: 120,
-}
-const mongoClient = mongoose.createConnection('mongodb://socket:qwe123123@13.209.42.70:27017/socket', options);
-const Schema = mongoose.Schema;
-const Socket = new Schema({
-    name: { type: String, default: 'hahaha' },
-    age: { type: Number, min: 18, index: true },
-    comments: { type: Array, default: [] },
-});
-var BlogPost = mongoClient.model('Socket', Socket);
-const getList = async () => {
-    let list = []
-    let cursor = BlogPost.find({}).cursor()
-    for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-        // Prints "Val" followed by "Varun"
-        list.push(doc);
-    }
-    return list
-}
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 var ObjectID = require("bson-objectid");
 let id = ObjectID();
-var post = new BlogPost({ 'name': id });
 chat.emit('login', { 'id': id });
 rl.setPrompt(id + '> ');
 rl.prompt();
@@ -44,8 +16,6 @@ rl.on('line', async function (line) {
         chat.emit('broadcast', { 'id': id, 'value': line })
         process.exit(0)
     } else {
-        post.comments.push(line);
-        await post.save();
         chat.emit('callback', { 'username': id, 'value': line });
         rl.setPrompt(id + '> ');
         await rl.prompt();
